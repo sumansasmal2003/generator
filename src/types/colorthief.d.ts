b4ba6e9c4931d14f0a7536b40ade52a53a7460b4
SUMAN SASMAL <sasmalsuman04@gmail.com>
2026-01-08 09:16:28 +0530

    some

---
+++ src/types/colorthief.d.ts
@@ -0,0 +1,7 @@
+// src/types/colorthief.d.ts
+declare module 'colorthief' {
+  export default class ColorThief {
+    getColor(sourceImage: HTMLImageElement, quality?: number): number[];
+    getPalette(sourceImage: HTMLImageElement, colorCount?: number, quality?: number): number[][];
+  }
+}
